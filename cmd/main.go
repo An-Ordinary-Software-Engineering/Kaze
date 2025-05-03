42301e0186ec0fb6a6f3f2bef1fd860be830a28f
csvitor-dev <csvitor.dev@gmail.com>
2025-05-02 21:55:44 -0300

    chore+refactor: change project stack from .NET to Golang

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Kaze - build .NET applications quickly!")
+}
--- src/Kaze.CLI/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using Kaze.Application;
-
-var root = CLIApplication.Build();
-
-return await CLIApplication.InvokeAsync(root, args);
